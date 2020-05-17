c47ffa17a00312de990d91a0504dfe2e784770dc
u0-blip <chenduona@gmail.com>
2020-05-17 13:05:50 +1000

    finished task3

--- task2/get_RFM.py
+++ task2/get_RFM.py
@@ -17,7 +17,7 @@ from sklearn.model_selection import KFold, cross_val_score, train_test_split
 import yaml
 from sklearn.metrics import silhouette_score
 
-with open('param.yaml', 'rb') as f:
+with open('C:\\Users\\peter\\source\\KPMG-virtual-internship\\task2\\param.yaml', 'rb') as f:
     params = yaml.load(f)
 
 train = int(params['train'])
@@ -60,7 +60,7 @@ def get_clust(data, field, num_cluster, order=True, min_cluster=2,  mode='manual
 
 
 def get_freq(tx_user,transaction):
-    tx_frequency = transaction.groupby('customer_id').transaction_date.count().reset_index()
+    tx_frequency = transaction.groupby('customer_id').td.count().reset_index()
 
     tx_frequency.columns = ['customer_id','Frequency']
     tx_frequency.head()
@@ -78,7 +78,7 @@ def get_profit(tx_user,transaction):
     return get_clust(tx_user, 'profit', params['profit_cluster'], True)
 
 def get_recency(tx_user,transaction):
-    tx_max_purchase = transaction.groupby('customer_id').transaction_date.max().reset_index()
+    tx_max_purchase = transaction.groupby('customer_id').td.max().reset_index()
     tx_max_purchase.columns = ['customer_id','MaxPurchaseDate']
     tx_max_purchase['Recency'] = (tx_max_purchase['MaxPurchaseDate'].max() - tx_max_purchase['MaxPurchaseDate']).dt.days
     tx_user = pd.merge(tx_user, tx_max_purchase[['customer_id','Recency']], on='customer_id')
@@ -95,7 +95,7 @@ def seg_cluster(tx_user, transaction, back_in_time):
 
 def seg_cluster1(tx_user, transaction, back_in_time):
     decay = 1.084
-    dates_customer = transaction['transaction_date']
+    dates_customer = transaction['td']
     if back_in_time:
         dates = (dates_customer.max() - dates_customer).dt.days
     else:
@@ -116,9 +116,10 @@ def seg_cluster1(tx_user, transaction, back_in_time):
 def get_RFM(transaction, back_in_time):
     user = pd.DataFrame(transaction['customer_id'].unique())
     user.columns = ['customer_id']
+    transaction.td = transaction.td.apply(pd.to_datetime)
     user = get_freq(user, transaction)
     user = get_recency(user, transaction)
     user = get_profit(user, transaction)
     # user = seg_cluster(user, transaction, back_in_time)
-    user = seg_cluster1(user, transaction, back_in_time)
+    user = seg_cluster(user, transaction, back_in_time)
     return user
